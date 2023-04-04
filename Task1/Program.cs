9fb11dc4e6ba040dcb8958564f4b8ade0f2070f8
Professional <Professional@USER-PC>
2023-04-04 23:13:32 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Solution
+    {
+        public int ClimbStairs(int n)
+        {
+            if (n <= 2)
+            {
+                if (n == 1)
+                {
+                    return 1;
+                }
+                if (n == 2)
+                {
+                    return 2;
+                }
+            }
+            int[] array = new int[n];
+            array[1] = 2;
+            array[2] = 3;
+            for (int i = 3; i <= n - 1; i++)
+            {
+                array[i] = array[i - 1] + array[i - 2];
+            }
+            return array.Max();
+        }
+    }
+}
